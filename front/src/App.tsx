41238a0c4f01cc9487ec7e04f1613a26804bcb53
Zeljko <ikanoviczeljko095@gmail.com>
2025-01-12 23:30:29 +0100

    dodat front

---
+++ front/src/App.tsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import './index.css'
+
+function App() {
+ 
+
+  return (
+  <>
+  <Navbar/>
+  <Home/>
+  <Footer/>
+  </>  
+  
+   
+     
+  )
+}
+
+export default App
